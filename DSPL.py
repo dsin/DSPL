1c53f117d4164b5d78e7c2dd7b1a0a70812c9fbe
dsin <phong.ek@gmail.com>
2013-09-20 12:47:19 +0700

    DSPL compiler first launched

---
+++ DSPL.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import sys
+
+if len(sys.argv) == 1:
+    print '''USAGE : python DEPL <filename>'''
+else :
+    print '''Hello Stupid guy.
+             Have a nice day.'''
+             
+# "I listen but I am not understand. What are you talking about ?" said the compiler
